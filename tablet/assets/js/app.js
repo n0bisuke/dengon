c4986b755d94f9d83dacecd5a284169f4f750567
omiyusuke <omi_yusuke@liginc.co.jp>
2015-10-27 03:17:45 +0900

    tablet用ファイルを追加

---
+++ tablet/assets/js/app.js
@@ -0,0 +1,13 @@
+"use strict";
+
+//milkcocoa
+var milkcocoa = new MilkCocoa("juiceig81r3wr.mlkcca.com");
+var ds = milkcocoa.dataStore('dengon');
+var session = new QiSession();
+
+ds.on('push', function(res){
+    session.service("ALMemory").done(function (ALMemory) {
+      console.log("ALMemory取得成功");
+      ALMemory.raiseEvent("PepperQiMessaging/fromtablet", res.value);
+    });
+});
