c69bd6fa47326f2b8264a8bb19bdfd3a945214e4
omiyusuke <omi_yusuke@liginc.co.jp>
2015-10-27 04:16:03 +0900

    顔認証テスト

--- tablet/assets/js/app.js
+++ tablet/assets/js/app.js
@@ -4,13 +4,25 @@
 var milkcocoa = new MilkCocoa("juiceig81r3wr.mlkcca.com");
 var ds = milkcocoa.dataStore('dengon');
 var session = new QiSession();
+var speech;
 
 ds.on('push', function(res){
     session.service("ALMemory").done(function (ALMemory) {
       console.log("ALMemory取得成功");
       ALMemory.raiseEvent("PepperQiMessaging/fromtablet", res.value);
     });
-    session.service("ALTextToSpeech").done(function(tts) {
-        tts.say(res.value.message);
-    });
+});
+
+session.service("ALTextToSpeech").done(function(tts) {
+    speech = tts;
+});
+
+session.service("ALFaceDetectionProxy").done(function(tts) {
+    tts.setRecognitionEnabled(true);
+});
+
+session.service("ALMemory").done(function (ALMemory) {
+  ALMemory.raiseEvent("FaceDetected", function(res){
+      speech.say(res[1][0]);
+  });
 });
